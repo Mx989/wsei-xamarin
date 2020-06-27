04885fbbd0ebd6021b7a8dd0e3f9c426a6b8b764
Maciej Bednarek <maciek.b.98@o2.pl>
2020-06-27 16:50:38 +0200

    1.4 & 1.5

---
+++ AirMonitor/AirMonitor/Views/Resources/Colors.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor.Views.Resources
+{
+
+}
