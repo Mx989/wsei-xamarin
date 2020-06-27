b98cabdf4a0d7674271332a294bc1c9ee067509e
Maciej Bednarek <maciek.b.98@o2.pl>
2020-06-27 17:00:08 +0200

    2

---
+++ AirMonitor/AirMonitor/Views/HomePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void NavigateToDetails(object sender, EventArgs e)
+        {
+            var page = new DetailsPage();
+            await Navigation.PushAsync(page);
+        }
+    }
+}
